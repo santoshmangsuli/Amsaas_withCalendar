7bc167a8c278f39ffd819aa086d9342789fa70d6
Santosh <santoshmangsuli@gmail.com>
2015-06-13 08:55:21 +0530

    First commit

---
+++ AmsaaSV0.1/src/main/webapp/lib/raghs-core/dispatcher.js
@@ -0,0 +1,41 @@
+/* 
+ * Document   : JS
+ * Author     : Raghavendra I Badiger
+ * Description: 
+ *  
+ */
+
+define(function(require) {
+	"use strict";
+	var urlToControllerMap = require("UrlMap"); 
+
+	return {
+		init : function(applnContext) {
+			var applicationContext=applnContext;
+			var dispatcherContext = this;
+			console.log("dispatcher init ");
+			$("#appContent a").click(function() {
+				
+				var url = $(this).attr('href');
+				if(url == '#billpayments'){
+					url = "#billDashBoardDiv";
+					console.log("loadModule if "+url);
+				}
+				console.log("loadModule "+url);
+				dispatcherContext.loadModule(url);
+			});
+		},
+
+		forward : function(url) {
+			this.loadModule('#' + url);
+		},
+
+		loadModule : function(url) {
+			console.log("loadModule "+url);
+			var moduleController = urlToControllerMap[url];
+			moduleController.init(this);
+		}
+
+	};
+
+});
